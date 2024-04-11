ed81b18cd400b8288b755739f0220baa0c810b0c
Nazasif92 <160417550+Nazasif92@users.noreply.github.com>
2024-04-11 08:31:58 +0500

    commit

---
+++ Q_12/main.js
@@ -0,0 +1,6 @@
+var names = ["wali", "ramzan", "sohail", "shezi"];
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log('Hello $(name),would you like to have a cup of tea?');
+}
+;
---
+++ Q_12/main.ts
@@ -0,0 +1,2 @@
+let names: string[] = ["wali","ramzan","sohail","shezi"];
+for (let name of names) {console.log('Hello $(name),would you like to have a cup of tea?')};
