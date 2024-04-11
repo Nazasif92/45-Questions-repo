48ae9a2e0b5fedadaf17741f94f7cf2e452bb5b5
Nazasif92 <160417550+Nazasif92@users.noreply.github.com>
2024-04-11 08:44:40 +0500

    commit

---
+++ Q_13/main.js
@@ -0,0 +1,2 @@
+var transports = ["Suzuki motorcycle", "Toyota car", "Bianchi bicycle"];
+transports.forEach(function (transport) { console.log("I would like to own a ".concat(transport)); });
---
+++ Q_13/main.ts
@@ -0,0 +1,3 @@
+let transports: string[] = ["Suzuki motorcycle", "Toyota car", "Bianchi bicycle"];
+
+transports.forEach(transport => {console.log(`I would like to own a ${transport}`)});
