4be52ffed843b0cc58f2e9db14aecf95397d5f30
Nazasif92 <160417550+Nazasif92@users.noreply.github.com>
2024-05-03 21:34:05 +0500

    commit

---
+++ Q_35/main.ts
@@ -0,0 +1,7 @@
+let animals:string[]=["Parrot","Horse","Dog"];
+animals.forEach(animal => {
+    console.log(`A ${animal} would make a great pet.`)
+});
+console.log("Any of these animals would make a great pet.");
+
+    
